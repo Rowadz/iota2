d83492dd593443720b9fd9a25bafad0064bee0c9
Rowad <rowadwo@gmail.com>
2019-11-22 10:32:19 +0200

    on color selectors

--- src/app/components/options/components/color-selector/color-selector.component.ts
+++ src/app/components/options/components/color-selector/color-selector.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import dialogPolyfill from "dialog-polyfill";
+import { v4 } from "uuid";
 
 @Component({
-  selector: 'iota-color-selector',
-  templateUrl: './color-selector.component.html',
-  styleUrls: ['./color-selector.component.scss']
+  selector: "iota-color-selector",
+  templateUrl: "./color-selector.component.html",
+  styleUrls: ["./color-selector.component.scss"]
 })
 export class ColorSelectorComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  @Input() isBg: boolean;
+  readonly id: string;
+  constructor() {
+    this.id = v4();
   }
 
+  ngOnInit() {}
+
+  openModal(): void {
+    const dialog: HTMLElement = document.getElementById(this.id);
+    dialogPolyfill.registerDialog(dialog);
+    (dialog as any).showModal();
+  }
 }
